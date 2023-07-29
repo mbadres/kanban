3ca5fbe39978727d08fe016ef85cc9bda549633a
Miena Basta Badres <dev@mbadres.de>
2023-07-29 02:07:53 +0200

    Initialize firebase.

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'kanban-bd21b',
+    appId: '1:925761979866:web:751c807fbdea24dfd36ce3',
+    storageBucket: 'kanban-bd21b.appspot.com',
+    apiKey: 'AIzaSyDKqpis_50Z0tvZkJOd-ZGniUU4sbLIb8k',
+    authDomain: 'kanban-bd21b.firebaseapp.com',
+    messagingSenderId: '925761979866',
+  },
+};
